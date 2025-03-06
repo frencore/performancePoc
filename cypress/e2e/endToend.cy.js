78a8902ed1ead9442f6e0a1bb04ac6c25bbb17fa
Felipe Rencoret. <felipe.antonio.rencoret@gmail.com>
2025-03-06 12:53:09 -0300

    Implementar Page Object Model (POM) y comandos personalizados para mejorar la reutilización y mantenimiento de las pruebas. Centralizar selectores y datos en un archivo JSON para una mejor gestión de las pruebas.

--- cypress/e2e/endToend.cy.js
+++ cypress/e2e/endToend.cy.js
@@ -1,41 +1,46 @@
 describe("Búsqueda de Pokémon en PokeAPI", () => {
-    it("Debe buscar un Pokémon usando el campo de búsqueda", () => {
-      // Constantes para los selectores y valores repetidos
-      const urlInput = "#url-input";
-      const searchButton = 'button[type="submit"]';
-      const messageElement = ".ApiExplorer-module__message--e4oy9";
-      const jsonViewer = ".JsonViewer-module__code--2XOHn";
-      const rawJsonInput = 'label > input';
-  
-      const pokemon = "pokemon/pikachu"; // Pokémon que estamos buscando
-  
+  it("Debe buscar un Pokémon usando el campo de búsqueda", () => {
+    // Cargar los datos del archivo JSON
+    cy.fixture("example.json").then((pageData) => {
+      // Extraer selectores y valores del JSON
+      const {
+        urlInput,
+        searchButton,
+        messageElement,
+        jsonViewer,
+        rawJsonInput,
+      } = pageData.selectors;
+      const { name, id } = pageData.pokemon;
+
       // Visitar la página de PokeAPI
       cy.visit("/");
-  
+
       // Encontrar el campo de búsqueda y escribir el nombre del Pokémon
-      cy.get(urlInput).clear().type(pokemon);
-  
-      // Hacer clic en el botón de búsqueda 
+      cy.get(urlInput)
+        .clear()
+        .type("pokemon/" + name);
+
+      // Hacer clic en el botón de búsqueda
       cy.get(searchButton).click();
-  
+
       // Verificar que el resultado contiene el nombre del Pokémon
-      cy.get(messageElement).should("include.text", "pikachu");
-  
+      cy.get(messageElement).should("include.text", name);
+
       // Hacer clic en 'View Raw JSON'
       cy.get(rawJsonInput).click();
-  
+
       // Usamos el comando personalizado para guardar el JSON
-      cy.guardarJsonDesdeElemento(jsonViewer, 'jsonResponse');
-  
+      cy.guardarJsonDesdeElemento(jsonViewer, "jsonResponse");
+
       // Usamos el alias para obtener el JSON y realizar aserciones
-      cy.get('@jsonResponse').then((jsonResponse) => {
+      cy.get("@jsonResponse").then((jsonResponse) => {
         // Imprimir el JSON extraído para verificar
-        cy.log('JSON Response:', jsonResponse); 
-  
+        cy.log("JSON Response:", jsonResponse);
+
         // Ejemplo de aserción sobre el JSON (ajustar según la estructura real)
-        expect(jsonResponse).to.have.property('name', 'pikachu');
-        expect(jsonResponse).to.have.property('id', 25); // Suponiendo que el id de Pikachu es 25
+        expect(jsonResponse).to.have.property("name", name);
+        expect(jsonResponse).to.have.property("id", id); // Suponiendo que el id de Pikachu es 25
       });
     });
   });
-  
+});
